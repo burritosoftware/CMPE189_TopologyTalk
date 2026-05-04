5518164e6a054d306df5c24ff204f8ab1f3be8ae
Rooosti <rusticodc@gmail.com>
2026-05-03 23:05:17 -0700

    added custom simple_switch to have packet-forwarding capability; rest_qos interferes with normal simple_switch

--- run_ryu.sh
+++ run_ryu.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # Based on README.md instructions but including QoS and Conf Switch for tool support
-ryu-manager --observe-links \
-    ryu/ryu/app/rest_topology.py \
-    ryu/ryu/app/rest_qos.py \
-    ryu/ryu/app/rest_conf_switch.py \
-    ryu/ryu/app/ofctl_rest.py
+ryu-manager \
+  --ofp-tcp-listen-port 6633 \
+  --observe-links \
+  ryu/ryu/app/qos_simple_switch_13.py \
+  ryu/ryu/app/rest_topology.py \
+  ryu/ryu/app/rest_conf_switch.py \
+  ryu/ryu/app/ofctl_rest.py \
+  ryu/ryu/app/rest_qos.py
